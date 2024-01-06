17c6c203ab1fc25f675047337435739dc5ff4358
rahuljat1349 <rahuldudi1349@gmail.com>
2024-01-06 16:49:36 +0530

    learnt route

--- 34_events.js
+++ 34_events.js
@@ -11,19 +11,19 @@ event.on("APIrequest", () => {
   console.log("API request count is :" + count);
 });
 
-app.get("/", (req, resp) => {
-  resp.send("You requested");
-  event.emit("APIrequest");
-});
-
-app.put("/", (req, resp) => {
-  resp.send("You requested");
-  event.emit("APIrequest");
-});
-
-app.post("/", (req, resp) => {
-  resp.send("You requested");
-  event.emit("APIrequest");
-});
+app
+  .route("/")
+  .get((req, resp) => {
+    resp.send("You requested");
+    event.emit("APIrequest");
+  })
+  .put((req, resp) => {
+    resp.send("You requested");
+    event.emit("APIrequest");
+  })
+  .post((req, resp) => {
+    resp.send("You requested");
+    event.emit("APIrequest");
+  });
 
 app.listen(5000);
